c3353949da7a165a036144c426aa8067d0e84a91
Andrico <andrico1234@yahoo.co.uk>
2017-02-23 01:26:21 +0000

    implement functionality to add notes

--- notes.js
+++ notes.js
@@ -1,10 +1,33 @@
 "use strict";
+const fs = require('fs');
 
 console.log('Starting notes.js');
 
 var addNote = (title, body) => {
 
     console.log(`Adding ${title} note, with content: ${body}`);
+    var notes = [];
+    var note = {
+
+        body: body, title: title,
+    };
+
+    if (fs.existsSync('notes-data.json')) {
+
+        var notesString = fs.readFileSync('notes-data.json');
+        notes = JSON.parse(notesString);
+    }
+
+    var duplicateNotes = notes.filter((note) => note.title === title);
+
+    if (duplicateNotes.length === 0) {
+
+        notes.push(note);
+        fs.writeFileSync(`notes-data.json`, JSON.stringify(notes));
+    } else {
+
+        console.log('Note title already exists');
+    }
 };
 
 var listNote = () => {
@@ -22,10 +45,7 @@ var removeNote = (title) => {
     console.log(`Removing ${title} from list`);
 };
 
-module.exports =  {
+module.exports = {
 
-    addNote: addNote,
-    listNote: listNote,
-    readNote: readNote,
-    removeNote: removeNote
+    addNote: addNote, listNote: listNote, readNote: readNote, removeNote: removeNote
 };
